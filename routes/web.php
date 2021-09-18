343881504f30d531830ad1316c49d567340b5984
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-09-18 15:04:27 +0900

    Herokuデプロイのための設定

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\ItemController@index');
+
 
 Route::resource('items', App\Http\Controllers\ItemController::class);
